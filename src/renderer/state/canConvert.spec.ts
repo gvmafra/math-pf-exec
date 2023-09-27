b0e8e879c59883ef825bad8afd64383265afb153
GVMafra <gvmafra@outlook.com>
2023-09-27 18:55:32 -0400

    Pintando Frações v.6.5.0 - after clean-up

--- src/renderer/state/canConvert.spec.ts
+++
@@ -1,8 +0,0 @@
-const isDecimal = (x: number) => x % 1 > Number.EPSILON;
-type Fraction = [numerator: number, denominator: number];
-function canConvert(userFrac: Fraction, targetDenominator: number): boolean {
-  const [n, d] = userFrac;
-
-  const numeratorIfFracHadTargetDenominator = (n * targetDenominator) / d;
-  return !isDecimal(numeratorIfFracHadTargetDenominator);
-}
