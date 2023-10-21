bb609c548c7b7fe198b6e3730b0c8100bd3d4e6e
GVMafra <gvmafra@outlook.com>
2023-10-21 12:30:41 -0400

    changed icons

---
+++ src/renderer/img/iconSplitter.tsx
@@ -0,0 +1,16 @@
+const IconHorizontal = () => (
+  <svg id="e" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 150 150">
+    <path d="M16.57 67.99h116.85v14.01H16.57Z" />
+  </svg>
+);
+
+const IconVertical = () => (
+  <svg id="e" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 150 150">
+    <path
+      transform="translate(150 0) rotate(90)"
+      d="M16.57 67.99h116.85v14.01H16.57Z"
+    />
+  </svg>
+);
+
+export { IconHorizontal, IconVertical };
