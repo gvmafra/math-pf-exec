adce3bd44279187a17cd2985cf0e515407302524
GVMafra <gvmafra@outlook.com>
2023-11-17 17:23:44 -0300

    minor

--- src/renderer/components/AdvanceStage.tsx
+++ src/renderer/components/AdvanceStage.tsx
@@ -1,7 +1,7 @@
 // src/renderer/components/AdvanceStage.tsx
 import { Action } from 'renderer/state/gameReducer';
 import { Dispatch } from 'react';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import { Link, useNavigate } from 'react-router-dom'; // Import useNavigate
 import { GameState } from 'renderer/state/types';
 import StagesCompletedList from './StagesCompletedList';
 
@@ -20,22 +20,30 @@ function AdvanceStage({ state, dispatch }: AdvanceStageProps) {
 
   return (
     <div className="flex h-screen bg-cyan-200">
-      <div className="flex flex-col p-6 gap-12 items-center content-center m-auto">
+      <div className="flex flex-col p-6 gap-6 items-center content-center m-auto">
+
         <h1 className="text-black text-2xl font-bold text-center">
           Parabéns! Você passou de estágio!
         </h1>
 
-        <div className='w-[300px] flex bg-amber-100 rounded-2xl border-2 border-black'>
+        <div className="w-[300px] flex bg-amber-100 rounded-2xl drop-shadow-md">
           <StagesCompletedList stages={state.stages} />
         </div>
 
-        <button
-          className="w-[300px] bg-amber-100 hover:bg-amber-600 hover:text-white text-black font-bold py-2 px-8 border-2 border-black rounded-xl"
-          onClick={handleClick}
-          aria-label="Move to next stage"
-        >
-          Próximo Estágio
-        </button>
+        <div className='flex w-[300] gap-4 items-center justify-between'>
+          <button
+            className=" bg-amber-100 hover:bg-amber-600 hover:text-white text-black font-bold py-2 px-8 drop-shadow-md rounded-xl"
+            onClick={handleClick}
+            aria-label="Move to next stage"
+          >
+            Continuar
+          </button>
+
+          <button className=" bg-amber-100 hover:bg-amber-600 hover:text-white text-black font-bold py-2 px-8 drop-shadow-md rounded-xl">
+            <Link to="/AllStagesPage">Estágios</Link>
+          </button>
+        </div>
+
       </div>
     </div>
   );
--- src/renderer/components/BeginPage.tsx
+++ src/renderer/components/BeginPage.tsx
@@ -10,17 +10,17 @@ const BeginPage: React.FC = () => {
         <h1 className="font-bold text-center mb-4 text-2xl">
           - PINTANDO FRAÇÕES -
         </h1>
-        <div className="bg-amber-300 rounded-3xl flex flex-col items-center p-8 border-4 gap-8 border-black">
+        <div className="bg-amber-300 rounded-3xl flex flex-col items-center p-8 gap-8 border-0 drop-shadow-md">
           <input
             type="text"
             placeholder="Enter your name..."
-            className="rounded-xl px-4 py-2 border-2 border-black"
+            className="rounded-xl px-4 py-2 w-full"
           />
-          <div className="flex items-center content-center gap-2">
-            <button className="bg-amber-100 hover:bg-amber-600 hover:text-white text-black font-bold py-2 px-8 border-2 border-black rounded-xl">
+          <div className="flex items-center content-center gap-4">
+            <button className="bg-amber-100 hover:bg-amber-600 hover:text-white text-black font-bold py-2 px-8 rounded-xl">
               <Link to="/Game">Começar</Link>
             </button>
-            <button className="bg-amber-100 hover:bg-amber-600 hover:text-white text-black font-bold py-2 px-8 border-2 border-black rounded-xl">
+            <button className="bg-amber-100 hover:bg-amber-600 hover:text-white text-black font-bold py-2 px-8 rounded-xl">
               <Link to="/AllStagesPage">Estágios</Link>
             </button>
           </div>
