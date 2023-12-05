3e77c13ff5314881beeb87ba8fdb085295e0a013
GVMafra <gvmafra@outlook.com>
2023-12-05 19:04:20 -0300

    testing for grid canvas scoring to work

--- src/renderer/state/gameReducer.ts
+++ src/renderer/state/gameReducer.ts
@@ -92,16 +92,17 @@ export default function gameStateReducer(
         6. The score should be a number between 0 and 3.
         7. Clicks include both toggling and untoggling cells, and clicking on the buttons that divide the canvas.
 
-        here's what I have so far, It doesn't work because it isn't accounting for the buttons that divide the canvas. It also doesn't account for the fact that the number of segments in the canvas can change.
+        here's what I have so far, It doesn't work because it isn't accounting for the buttons that divide the canvas. 
+        It also doesn't account for the fact that the number of segments in the canvas can change.
 
-         const minimumNumberOfClicks = Math.ceil(
-           challengeRatio * numberOfSegments
-         );
-         const currentNumberOfClicks = currentLevel.metadata.clickCount;
-         const difference = currentNumberOfClicks - minimumNumberOfClicks;
-         const score = Math.max(3 - difference, 0);
+        //  const minimumNumberOfClicks = Math.ceil(
+        //    challengeRatio * numberOfSegments
+        //  );
+        //  const currentNumberOfClicks = currentLevel.metadata.clickCount;
+        //  const difference = currentNumberOfClicks - minimumNumberOfClicks;
+        //  const score = Math.max(3 - difference, 0);
 
-         currentLevel.metadata.score = score as 1 | 2 | 3;
+        //  currentLevel.metadata.score = score as 1 | 2 | 3;
 
         what I need to do is to get the number of segments in the canvas after the canvas is divided, 
         and then use that number to calculate the minimum number of clicks required to complete the level.
@@ -131,11 +132,21 @@ export default function gameStateReducer(
         /*
         This works fine for the challenge canvas, but not for the grid canvas.
         I need to account for the fact that the number of points should never exceed 3.
-        The required minimum number of clicks should equal the number of cells toggled required to equal the challenge ratio, plus the amount of clicks required to divide the canvas.
+        The required minimum number of clicks should equal the number of cells toggled required to equal the challenge ratio, 
+        plus the amount of clicks required to divide the canvas.
+
         i.e.: 
-        If the challenge ratio is 0.5, and the canvas is divided into 2 cells, then the minimum number of clicks required to complete the level with 3 points is 3. (1 click to divide the canvas, and 2 clicks to toggle the cells).
-        If the challenge ratio is 0.5, and the canvas is blank, then the minimum number of clicks required to complete the level with 3 points is 2. (1 click to divide the canvas, and 1 click to toggle the cells).
-        If the challenge ratio is 2/3, and the canvas is divided into 2 cells, the minimum number of clicks required to complete the level with 3 points is 3. (1 click to divide the canvas, and 2 clicks to toggle the cells).
+        If the challenge ratio is 0.5, and the canvas is divided into 2 cells, 
+        then the minimum number of clicks required to complete the level with 3 points is 3. 
+        (1 click to divide the canvas, and 2 clicks to toggle the cells).
+        If the challenge ratio is 0.5, and the canvas is blank, then the minimum number of clicks required to complete the level with 3 points is 2. 
+        (1 click to divide the canvas, and 1 click to toggle the cells).
+        If the challenge ratio is 2/3, and the canvas is divided into 2 cells, the minimum number of clicks required to complete the level with 3 points is 3. 
+        (1 click to divide the canvas, and 2 clicks to toggle the cells).
+
+        In addition, canvas.numberOfDivisions should take care of the number of clicks required to divide the canvas.
+        If the user clicks the divide button, then canvas.numberOfDivisions should increase by 1. 
+        That should reflect the score, considering that the user should aim for the most efficient way of reaching the desired ratio.
         */
 
         break;
